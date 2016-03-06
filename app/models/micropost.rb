d483eaa67ad7e4937b1e35e92d8b23e33356f1f8
Rushil Basappa <rushil.oucse37@gmail.com>
2016-03-06 18:22:34 +0530

    Add user micropost

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,12 +4,11 @@ class Micropost < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
-  validates :picture_size
-
+  #validates :picture_size
   private
 
   def picture_size
-    if picture_size > 5.megabytes
+    if picture.size > 5.megabytes
       error.add(:picture, "Should be less than 5MB")
     end
   end
